d976c41e6ac57ec919ba512377c8b09d2836e436
lymei <lymeicontato@gmail.com>
2023-05-15 21:26:11 -0300

    feat: added about us text

--- app/sobre/page.js
+++ app/sobre/page.js
@@ -2,8 +2,21 @@ export default function Home() {
 	return (
 		<>
 			<main>
-				<div className='h-screen flex justify-center items-center'>
-					<h1 className='text-8xl p-16 rounded-lg bg-primary-two-300/50'>SOBRE NOS</h1>
+				<div className='h-screen flex flex-col justify-center items-center'>
+					<h1 className='text-8xl p-16'>Sobre Nós</h1>
+					<p>Bem-vindos à Korjata, a principal plataforma dedicada a unir talentosos programadores freelancers a projetos empresariais. Estamos aqui para oferecer uma solução eficiente e inovadora, facilitando o processo de encontrar talentos e projetos sob medida para atender às necessidades específicas de cada negócio.</p>
+
+					<p>Na Korjata, entendemos a importância de contar com desenvolvedores altamente qualificados e experientes. Sabemos também que encontrar os projetos certos pode ser um desafio tanto para programadores freelancers quanto para empresas em busca de soluções rápidas ou projetos completos. É por isso que criamos uma plataforma intuitiva e abrangente, onde talento e oportunidade se encontram.</p>
+
+					<p>Para programadores freelancers, a Korjata oferece acesso a uma ampla variedade de projetos, desde tarefas rápidas até projetos complexos e de longo prazo. Com uma interface amigável e um sistema de classificação, ajudamos você a encontrar os projetos ideais que se alinham às suas habilidades e interesses, permitindo que você maximize seu potencial e construa uma carreira sólida no mundo freelancer.</p>
+
+					<p>Já para empresas em busca de desenvolvedores freelancers, a Korjata oferece uma ampla gama de talentos altamente qualificados. Com um processo de seleção rigoroso e um sistema de avaliação, garantimos que você tenha acesso aos melhores programadores para impulsionar seus projetos e alcançar resultados excepcionais. Nosso objetivo é facilitar a contratação de freelancers de alta qualidade, economizando tempo e recursos para sua empresa.</p>
+
+					<p>Além disso, na Korjata, priorizamos a transparência, segurança e satisfação de nossos usuários. Mantemos um ambiente seguro e confiável, com opções de pagamento protegidas e suporte ao cliente dedicado para auxiliá-lo em todas as etapas do processo.</p>
+
+					<p>Seja você um programador freelancer em busca de novas oportunidades empolgantes ou uma empresa procurando talentos para desenvolver seus projetos, a Korjata é o seu destino ideal. Junte-se a nós hoje mesmo e faça parte de uma comunidade de sucesso, onde conexões valiosas são feitas e projetos de qualidade são concluídos.</p>
+
+					<p>Korjata - A plataforma para encontrar o talento certo e alcançar o sucesso em projetos empresariais.</p>
 				</div>
 			</main>
 		</>
