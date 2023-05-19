0a9b753c7b95a5710c2a8e696db2ea2316a65cb4
Lymei <lymeicontato@gmail.com>
2023-05-19 11:45:21 -0300

    style(Hero.jsx): added button style

--- app/components/Hero.jsx
+++ app/components/Hero.jsx
@@ -31,7 +31,15 @@ export default function Hero() {
 						Korjata é uma plataforma de conexão entre programadores freelancers e empresas em busca de
 						soluções.
 					</p>
-					{session ? <a href='/comecar'>Comece já!</a> : <SignInButton text={'Cadastre-se gratuitamente'} />}
+					{session ? (
+						<a
+							href='/comecar'
+							className='border border-primary-one-300 hover:bg-white/10 hover:scale-105 transition-all duration-300 rounded-lg px-4 py-2 w-fit'>
+							Comece já!
+						</a>
+					) : (
+						<SignInButton text={'Cadastre-se gratuitamente'} />
+					)}
 				</div>
 				<div className='relative max-sm:w-[50vw] max-sm:h-[50vw] max-sm:mx-auto col-span-3'>
 					<Image
--- app/components/SignInButton.jsx
+++ app/components/SignInButton.jsx
@@ -6,7 +6,7 @@ function SignInButton({ text }) {
 	return (
 		<button
 			onClick={signIn}
-			className='bg-primary-one-300 hover:bg-primary-one-200 hover:scale-105 transition-all duration-300 rounded-lg px-4 py-2 w-fit'>
+			className='border border-primary-one-300 hover:bg-white/10 hover:scale-105 transition-all duration-300 rounded-lg px-4 py-2 w-fit'>
 			{text}
 		</button>
 	);
